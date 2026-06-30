a87e265fb84a55c083304d4f122017bb75b5a010
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-06-30 06:06:40 +0000

    ユーザーページ全体実装

---
+++ db/migrate/20260626082231_add_profile_fields_to_users.rb
@@ -0,0 +1,6 @@
+class AddProfileFieldsToUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :address, :string
+    add_column :users, :introduction, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_06_26_061119) do
+ActiveRecord::Schema[8.0].define(version: 2026_06_26_083514) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -28,6 +28,9 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_26_061119) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.text "introduction"
+    t.string "postal_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
