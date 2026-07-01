0962967a88eee9f76e366aae916ba17bcac42605
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-07-01 00:26:09 +0000

    画像アップロード機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     keys = %i[name postal_code address self_introduction]
     devise_parameter_sanitizer.permit(:sign_up, keys:)
-    devise_parameter_sanitizer.permit(:account_update, keys:)
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :avatar])
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
   def index
     @users = User.order(:id).page(params[:page])
@@ -7,5 +5,25 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Add User'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.fetch(:user, {}).permit(:name, :avatar)
   end
 end
