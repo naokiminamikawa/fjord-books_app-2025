73a09762375341ae89b1232db6c3553f4d31d1f0
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-07-01 06:42:52 +0000

    レポートの一覧・詳細画面の作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,4 +8,24 @@ class ReportsController < ApplicationController
   def show
     @report = Report.find(params[:id])
   end
+  
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+
+    if @report.save
+      redirect_to @report, notice: 'Report was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :body)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_07_01_051414) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_01_063110) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -51,6 +51,8 @@ ActiveRecord::Schema[8.0].define(version: 2026_07_01_051414) do
   create_table "reports", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
