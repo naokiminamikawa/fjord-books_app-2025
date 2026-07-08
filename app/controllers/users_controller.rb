7cc127de799675b8592f8a83cf2e32bdf938a55a
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-07-08 08:06:59 +0000

    ユーザー登録・更新機能を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,24 +8,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to root_url, notice: 'Add User'
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def user_params
-    params.fetch(:user, {}).permit(:name, :avatar)
-  end
 end
