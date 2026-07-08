2e965335847fc023fda50672019b52c48638c1a0
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-07-08 06:28:06 +0000

    ログイン後のリダイレクト先を本の一覧画面へ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(_resource)
-    edit_user_registration_path
+    books_path
   end
 
   def configure_permitted_parameters
