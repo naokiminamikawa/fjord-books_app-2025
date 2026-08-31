01216eb0bd20a011007c7014321ac3d409a7e698
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-08-31 04:24:40 +0000

    モデルテスト追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,22 +3,40 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test '作成者の場合はeditable?がtrueを返す' do
+  test 'returns true when the user is the author' do
     report = reports(:alice_report)
     user = users(:one)
 
     assert report.editable?(user)
   end
 
-  test '作成者以外の場合はeditable?がfalseを返す' do
+  test 'returns false when the user is not the author' do
     report = reports(:alice_report)
     user = users(:two)
 
     assert_not report.editable?(user)
   end
 
-  test 'created_onが作成日を返す' do
+  test 'returns the created date' do
     report = reports(:alice_report)
+
     assert_equal report.created_at.to_date, report.created_on
+  end
+
+  test 'save_mentions creates a ReportMention record' do
+    report = reports(:alice_report)
+
+    mentioned_report = Report.create!(
+      title: 'Rubyの勉強',
+      content: 'Rubyを学んだよ',
+      user: users(:two)
+    )
+
+    report.mentioning_reports << mentioned_report
+
+    assert ReportMention.exists?(
+      mentioning_id: report.id,
+      mentioned_id: mentioned_report.id
+    )
   end
 end
